dba3ea0ac3637b9cf7428a029ce858ee3b131650
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-29 15:19:38 -0600

    Add testing environment to project dependencies

---
+++ src/App/App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe('App', () => {
+  it('should be true', () => {
+    expect(true).toEqual(true);
+  })
+})
