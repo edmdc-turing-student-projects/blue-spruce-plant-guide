7be01e2a7947c582c047ffc3939b8941cfacb528
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-29 19:32:14 -0600

    Add some app structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './App.scss'
 
-class App extends Component {
-	render () {
-		return (
-		<div className={styles.main}>
-			<h1>My React App</h1>
-		</div>
-		);
-	}
+function App () {
+  return (
+  <div className={styles.main}>
+    <h1>My React App</h1>
+  </div>
+  );
 }
 
 export default App;
