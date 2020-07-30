91094343ea0a46d8c262a573731ab0ceb60dfbdc
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-29 22:04:41 -0600

    Add eslint rules

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,13 +1,17 @@
-const rootUrl = "trefle.io/api/v1"
-const apiToken = "token=wf2V-4m1I00HHw7D-CP1TjvqmirvMX6veHLLikMq8hY"
+export default function ApiCalls() {
+  const rootUrl = 'trefle.io/api/v1'
+  const apiToken = 'token=wf2V-4m1I00HHw7D-CP1TjvqmirvMX6veHLLikMq8hY'
 
-export async function getSomeNativePlants() {
-  try {
-  const  response = await fetch(`https://cors-anywhere.herokuapp.com/${rootUrl}/distributions/colorado/plants?filter%5Bestablishment%5D=native&${apiToken}&page=1`)
-  const plantDataPageOne = await response.json()
-  console.log(plantDataPageOne)
-  return plantDataPageOne
-  } catch(error) {
-    console.error(error)
+  return {
+    getSomeNativePlants: async () => {
+      try {
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/${rootUrl}/distributions/colorado/plants?filter%5Bestablishment%5D=native&${apiToken}&page=1`)
+        const plantDataPageOne = await response.json()
+        console.log(plantDataPageOne)
+        return plantDataPageOne
+      } catch (error) {
+        return { ...error }
+      }
+    }
   }
 }
