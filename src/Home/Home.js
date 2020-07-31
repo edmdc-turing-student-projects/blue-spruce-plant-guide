34a99f072b27fa160f49945c000b38328c5fa42f
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-07-30 20:45:22 -0600

    Add home component

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Home.scss'
+
+export default function Home() {
+  return (
+    <header>
+      <h1>Quiz By:</h1>
+      <button type="button">Images</button>
+      <button type="button">Scientific Name</button>
+    </header>
+  )
+}
