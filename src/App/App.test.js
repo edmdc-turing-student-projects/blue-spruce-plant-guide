b435c8ad272fe39e81be1be126c847e751e976d0
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-03 13:12:40 -0600

    Start second integration test reorganize some imports

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MemoryRouter as Router } from 'react-router-dom'
+import { mocked } from 'ts-jest/utils'
 import App from './App'
+import { getColoradoNativePlants, quizGenerator } from '../Utils/index'
 import mockPlantIndex from '../Utils/mockPlantIndex'
-import getColoradoNativePlants from '../Utils/ApiCalls'
+import mockQuizData from '../Utils/mockQuizData'
 
-jest.mock('../Utils/ApiCalls')
-getColoradoNativePlants.mockResolvedValue(mockPlantIndex)
+jest.mock('../Utils/index')
+// quizGenerator.mockImplementation(mockQuizData)
+// quizGenerator.mockImplementation(() => mockQuizData)
 
 describe('App navigation from landing page', () => {
+  getColoradoNativePlants.mockResolvedValue(mockPlantIndex)
   it('should navigate to plant index and display all plants', async () => {
-    const { getByRole, findAllByRole, debug } = render(
+    const { debug, getByRole, findAllByRole } = render(
       <Router>
         <App />
       </Router>
@@ -20,9 +24,30 @@ describe('App navigation from landing page', () => {
 
     expect(link2PlantIndex).toBeInTheDocument()
     fireEvent.click(link2PlantIndex)
-
     debug()
+    console.log(getColoradoNativePlants)
+    console.log([...mockPlantIndex])
+
     const plantGuide = await findAllByRole('figure')
     expect(plantGuide).toHaveLength(20)
+  })
+
+  it('should navigate to quiz and default to scientific name quiz mode', async () => {
+    const {
+      getByRole, findByRole, findAllByRole, debug
+    } = render(
+      <Router>
+        <App />
+      </Router>
+    )
+    const link2Quiz = getByRole('link', { name: 'Quiz' })
+
+    expect(link2Quiz).toBeInTheDocument()
+    fireEvent.click(link2Quiz)
+
+    const quizPrompt = await findByRole('heading', { name: 'Parnassia palustris' })
+    const quizAnswerChoices = await findAllByRole('button')
+    expect(quizPrompt).toBeInTheDocument()
+    expect(quizAnswerChoices).toHaveLength(4)
   })
 })
---
+++ src/Utils/index.js
@@ -0,0 +1,3 @@
+export { default as getColoradoNativePlants } from './ApiCalls'
+export { default as quizGenerator } from './quizGenerator'
+
