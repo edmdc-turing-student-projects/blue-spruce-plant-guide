a32eccf175eb64d154470418f51831c4fae79fdf
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-08-03 20:08:02 -0600

    Add ability to reset quiz from results page

--- src/QuizResult/QuizResult.js
+++ src/QuizResult/QuizResult.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
 import styles from './QuizResult.scss'
 
-export default function QuizResults({ username, score }) {
+export default function QuizResults({ username, score, startNewRound }) {
   return (
     <section className={styles.quizResults}>
       <h3>{username}</h3>
@@ -12,7 +13,14 @@ export default function QuizResults({ username, score }) {
         {' '}
         of 10 correct
       </h2>
-      <button type="submit" onClick={() => {}}>Start Another Quiz</button>
+      <NavLink to="/quiz">
+        <button
+          type="submit"
+          onClick={() => startNewRound()}
+        >
+          Start Another Quiz
+        </button>
+      </NavLink>
     </section>
   )
 }
